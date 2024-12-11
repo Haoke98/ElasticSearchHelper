786522b19cbc3b1014e0f7377b99456c8772055e
Sadam·Sadik <1903249375@qq.com>
2024-12-11 21:51:20 +0800

    Opt: 优化了ik分词器健康状态的代码逻辑和节点信息遍历逻辑

--- health-monitoring/check-analyzer-health.py
+++ health-monitoring/check-analyzer-health.py
@@ -9,9 +9,9 @@
 import os
 
 from dotenv import load_dotenv
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, SSLError, RequestError
 
-load_dotenv('/Users/shadikesadamu/SLRC/数据处理工具/python/.env')
+load_dotenv('/Users/shadikesadamu/.config/.env')
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 es = Elasticsearch(hosts=os.getenv('SLRC_ES_PROTOCOL') + "://" + os.getenv("SLRC_ES_HOST"),
                    http_auth=(os.getenv("SLRC_ES_USERNAME"), os.getenv("SLRC_ES_PASSWORD")),
@@ -22,41 +22,37 @@ def check():
     global es
     resp = es.cat.plugins()
     print(resp)
-    resp = es.cat.nodes(h="id,name,http_address")
-    data_nodes = {}
-    lines = resp.split("\n")
-    for line in lines:
-        line = line.strip()
-        if line == '':
-            break
-        i = 0
-        items = line.split(" ")
-        item_list = []
-        for item in items:
-            item = item.strip()
-            if item != '':
-                item_list.append(item)
-        data_nodes[item_list[0]] = {
-            "name": item_list[1],
-            "http_address": item_list[2],
-        }
-        pass
-    for node_id, node in data_nodes.items():
-        node_name = node["name"]
-        http_address = node['http_address']
+    nodes_info = es.nodes.info()
+    for node_id, node_info in nodes_info['nodes'].items():
+        node_name = node_info["name"]
+        http_address = node_info['http']['publish_address']
+        roles = node_info['roles']
+        print("NODE_ID:", node_id, http_address, node_name, roles, ':')
         es = Elasticsearch(hosts=os.getenv('SLRC_ES_PROTOCOL') + "://" + http_address,
-                           basic_auth=(os.getenv("SLRC_ES_USERNAME"), os.getenv("SLRC_ES_PASSWORD")),
+                           http_auth=(os.getenv("SLRC_ES_USERNAME"), os.getenv("SLRC_ES_PASSWORD")),
                            ca_certs=os.getenv("SLRC_ES_CA"), request_timeout=3600)
-        print(node_id, node_name, http_address, ":")
         analyzer_list = ["ik_max_word", "ik_smart"]
         for analyzer in analyzer_list:
-            resp = es.indices.analyze(
-                body={
-                    "analyzer": analyzer,  # 指定IK分词器
-                    "text": "这是要分析的文本"  # 待分析的文本
-                }
-            )
-            print("\t", analyzer, "\t", resp)
+            print("\t\t", str(analyzer).ljust(16, " "), end=':\t    ')
+            try:
+                resp = es.indices.analyze(
+                    body={
+                        "analyzer": analyzer,  # 指定IK分词器
+                        "text": "这是要分析的文本"  # 待分析的文本
+                    }
+                )
+                print("Ok!")
+            except SSLError as e:
+                print("证书异常")
+            except RequestError as e:
+                if e.status_code == 400:
+                    _error = e.info.get('error')
+                    root_case = _error.get('root_case')
+                    reason = _error.get('reason')
+                    if reason.startswith("failed to find global analyzer"):
+                        print("该节点没有分词器")
+                        continue
+                raise e
 
 
 def check_on_update():
