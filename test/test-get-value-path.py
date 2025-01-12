0406a79ed94dc0c0be40a175109b0e68f07f9dc4
Sadam·Sadik <1903249375@qq.com>
2025-01-13 05:15:34 +0800

    Opt&Feat: 优化了单元测试，实现了小规模定向测试
    Signed-off-by: Sadam·Sadik <1903249375@qq.com>

--- test/test-get-value-path.py
+++ test/test-get-value-path.py
@@ -1,23 +1,18 @@
-import json
 import os
 
 from elasticsearch import Elasticsearch
 
 from es_helper.map.reindex import load_field_mapping, transform_doc, bulk_operation
 
-if __name__ == '__main__':
-    # 获取源字段的值
-    strict_mode = False
-    _id = '91410200769463127C'
-    src_index = 'ent-mdsi-v4'
-    dst_index = 'ent-mdsi-v5'
-    src_field = 'jobInfoData.totalNum'
-    esCli = Elasticsearch(hosts=os.getenv('SLRC_ES_PROTOCOL') + "://" + os.getenv("SLRC_ES_HOST"),
-                          http_auth=(os.getenv("SLRC_ES_USERNAME"), os.getenv("SLRC_ES_PASSWORD")),
-                          ca_certs=os.getenv("SLRC_ES_CA"), request_timeout=3600)
-    doc = esCli.get(index=src_index, id=_id)
-    print(json.dumps(doc.body, indent=4, ensure_ascii=False))
+esCli = Elasticsearch(hosts=os.getenv('SLRC_ES_PROTOCOL') + "://" + os.getenv("SLRC_ES_HOST"),
+                      basic_auth=(os.getenv("SLRC_ES_USERNAME"), os.getenv("SLRC_ES_PASSWORD")),
+                      ca_certs=os.getenv("SLRC_ES_CA"), request_timeout=3600)
 
+
+def small_scale_testing(_id, src_index: str, dst_index: str):
+    print(_id, end=": ")
+    doc = esCli.get(index=src_index, id=_id)
+    # print(json.dumps(doc.body, indent=4, ensure_ascii=False))
     # 加载字段映射
     field_mapping = load_field_mapping(r"D:\Projects\IndexMap\reindex_v4_to_v5.csv")
     transformed_doc = transform_doc(doc=doc, field_mapping=field_mapping, strict_mode=strict_mode)
@@ -29,3 +24,15 @@ if __name__ == '__main__':
         '_source': transformed_doc
     })
     bulk_operation(esCli, action_buffer)
+    print("(Ok)")
+
+
+if __name__ == '__main__':
+    # 获取源字段的值
+    strict_mode = False
+    # _id =
+    # _id =
+    # _id =
+    target_documents = ['91410200769463127C', '91211121059812484C', '91350203MA344PKP8K']
+    for _id in target_documents:
+        small_scale_testing(_id, 'ent-mdsi-v4', 'ent-mdsi-v6')
