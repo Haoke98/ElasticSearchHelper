71e7b7c2c9d94140634a731ff7c44d2eb8a2a371
Sadam·Sadik <1903249375@qq.com>
2025-01-14 00:21:58 +0800

    Feat: 已经实现了按不同的维度覆盖率进行小规模单元测试
    Signed-off-by: Sadam·Sadik <1903249375@qq.com>

--- test/test-reindex-docs-with-nested-field.py
+++ test/test-reindex-docs-with-nested-field.py
@@ -1,7 +1,6 @@
 import os
-
 from elasticsearch import Elasticsearch
-
+from tqdm import tqdm
 from es_helper.map.reindex import load_field_mapping, transform_doc, bulk_operation
 
 esCli = Elasticsearch(hosts=os.getenv('SLRC_ES_PROTOCOL') + "://" + os.getenv("SLRC_ES_HOST"),
@@ -9,22 +8,35 @@ esCli = Elasticsearch(hosts=os.getenv('SLRC_ES_PROTOCOL') + "://" + os.getenv("S
                       ca_certs=os.getenv("SLRC_ES_CA"), request_timeout=3600)
 
 
-def test_nested_field(field_name: str):
-    resp = esCli.search(query={
+def test_nested_field(src_index: str, dst_index: str, field_name: str):
+    resp = esCli.search(index=src_index, query={
         "nested": {
             "path": field_name,
             "query": {
                 "match_all": {}
             }
         }
-    })
-    pass
+    }, size=1000)
+
     hits = resp["hits"]["hits"]
-    for hit in hits:
-        transform_doc(hit, field_mapping, False)
-        pass
+    total = len(hits)
+    print(f"找到 {total} 条文档需要处理")
+
+    # 使用tqdm创建进度条
+    action_buffer = []  # 清空缓冲区
+    for doc in tqdm(hits, desc="处理文档", unit="doc"):
+        transformed_doc = transform_doc(doc, field_mapping, False)
+        action_buffer.append({
+            '_index': dst_index,
+            '_id': doc['_id'],
+            '_source': transformed_doc
+        })
+    bulk_operation(esCli, action_buffer)
+    print("(Ok)")
 
 
 if __name__ == '__main__':
     field_mapping = load_field_mapping(r"D:\Projects\IndexMap\reindex_v4_to_v5.csv")
-    test_nested_field("newsFeelingsData")
+    for nested_field_name in ["newsFeelingsData.dataList", "administrativePenaltyData.dataList",
+                              "announcementData.dataList",]:
+        test_nested_field("ent-mdsi-v4", "ent-mdsi-v6", nested_field_name)
